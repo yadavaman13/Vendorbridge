6812fb0b56abb79b9ab5606b434b514fdc178368
yadavaman13 <yadavaman8511005211@gmail.com>
2026-06-06 13:58:09 +0530

    users controller

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,24 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import morgan from 'morgan';
-import envConfig from './config/envConfig.js';
-import authRoutes from './routes/auth.routes.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+import envConfig from "./config/envConfig.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin: envConfig.CLIENT_ORIGINS,
-        credentials: true,
-    }),
+  cors({
+    origin: envConfig.CLIENT_ORIGINS,
+    credentials: true,
+  }),
 );
-app.use(morgan('combined')); //  Logging middleware for better debugging
+app.use(morgan("combined")); //  Logging middleware for better debugging
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 export default app;
